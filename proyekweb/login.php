df8c7992ad446b80b2a994ccc214faacb311e385
sabrinamawadathun <sabrinamawadathun@gmail.com>
2025-09-21 11:22:48 +0800

    sabrina

---
+++ proyekweb/login.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Corinthia:wght@400;700&family=Pacifico&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>LOGIN</title>
+    <style>
+        body{
+            margin: 0;
+            font-family: arial, sans-serif;
+            display: flex;
+            height: 100hv;
+        }
+        h1{
+            margin-bottom: 10px;
+        }
+        p{
+            margin-bottom: 20px;
+        }
+        form{
+            width: 100%;
+            max-width: 300px;
+            display: flex;
+            flex-direction: column;
+        }
+        label{
+            margin: 10px 0 5px;
+            font-size: 14px;
+        }
+        input{
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+        button{
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    <div class="left">
+        <div class="logo">Logo</div>
+        <h1>LOG IN</h1>
+        <p>lorem ipsum dolor sit amet</p>
+        <form>
+            <label for="">Email*</label>
+            <input type="email"required>
+            <label for="">Password*</label>
+            <input type="password"required>
+            <div class="forgot"><a href="#">forgot your password?</a></div>
+            <button class="btn-primary" type="submit">log in</button>
+            <button class="btn-google" type="button" >
+                <i class='bx bxl-google'></i>
+                <div>log in with google</div>
+            </button>
+        </form>
+        <div class="signup">don't have a account?<a href="#">Sign up</a></div>
+        <div class="footer">©kelompok11</div>
+    </div>
+    <div class="right">
+        <div class="section-img">
+            <div class="section-img">
+                <img src="image/1.png" alt="">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- proyekweb/signup.php
+++ proyekweb/signup.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Sign Up</title>
-    <link rel="stylesheet" href="assets/css/styleup.css" />
+    <link rel="stylesheet" href="../assets/css/styleup.css" />
     <!-- CSS akan disimpan di file terpisah -->
   </head>
   <body>
