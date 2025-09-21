a6df5d12065c3b5a0a508f78aff8f2b2160aec21
sabrinamawadathun <sabrinamawadathun@gmail.com>
2025-09-21 11:08:09 +0800

    b

---
+++ proyekweb/menu.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Corinthia:wght@400;700&family=Pacifico&display=swap" rel="stylesheet">
+    <title>Rumah Makan Padang Sejahtera</title>
+    <style>
+        body{font-family: arial, sans-serif; margin:0; padding:0; line-height:1.6;}
+        header, footer{background: #f8f8f8; padding:15px 30px;}
+        header nav a{margin:0 10px; text-decoration:none; color:#333;}
+        header{display:flex; justify-content:space-between; align-items:center;}
+        footer{text-align:center; font-size:14px;}
+    </style>
+</head>
+<body>
+    <header>
+        <div class = "logo"><strong>Logo</strong></div>
+        <nav>
+            <a href="#">Menu Utama</a>
+            <a href="#">Promo Harian</a>
+            <a href="#">Galeri Makanan</a>
+            <a href="#">Link lain</a>
+        </nav>
+        <button>button</button>
+    </header>
+
+    <section class = "hero">
+        <div>
+            <h1>RUMAH MAKAN PADANG SEJAHTERA</h1>
+            <P>Nikmati cita rasa minang autentik di Rumah Makan Padang Sejahtera Setiap hidangan disiapkan dengan resep tradisional yang tak diragukan turun-temurun</P>
+            <a href="#">Lihat Menu</a>
+            <a href="#">Pesan Sekarang</a>
+        </div>
+        <img src="image/rumah.jpg" alt="Gambar">
+    </section>
+
+    <section class ="promo">
+        <div>
+            <h2>Nikmati Diskon Menarik Setap Minggu</h2>
+            <p>Dapatkan diskon 10% untuk rendang setiap jumat. Jangan lewatkan kesempatan ini untuk menikmati hidangan favorit Anda!</p>
+            <ul>
+                <li><strong>Rendang spesial</strong> Rendang lembut dan bercita rasa khas Minang siap memanjakan lidah Anda.</li>
+                <li><strong>Jangan lewatkan:</strong> Hidangan lezat dengan potongan menarik hanya untuk Anda setiap Jumat.</li>
+            </ul>
+            <a href="#" class="btn">Lihat</a>
+        </div>
+        <img src="image/rendang.jpg" alt="Promo Image">
+    </section>
+
+    <section class="menu">
+        <h2 style="text-align:center">Nikmati Beragam Menu Lezat yang SiapNikmati Beragam Menu Lezat yang Siap Memanjakan Lidah Anda</h2>
+        <div class="menu-items">
+            <div class="menu-item">
+                <img src="image/menu1.jpg" alt="menu 1">
+                <h3>Rasakan Cita Rasa Minang</h3>
+                <p>Kami menyajikan hidangan khas Padang dengan bahan berkualitas dan resep tradisonal.</p>
+            </div>
+            <div class="menu-item">
+                <img src="image/menu2.jpg" alt="Menu 2">
+                <h3>Promo menarik setiap hari</h3>
+                <p>Dapatkan diskon spesial dan penawaran menarik setiap harinya.</p>
+            </div>
+            <div class="menu-item">
+                <img src="image/menu3.jpg" alt="menu 3">
+                <h3>Galeri Makanan dan Suasana</h3>
+                <p>Lihat koleksi foto hidangan lezat dan suasana restoran yang menggoda</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="contact">
+        <h2>Kontak kami</h2>
+        <div class="contact-info">
+            <p><strong>Email:</strong><a href="mailto:info@padangsejahtera.com">info@padangsejahtera.com</a></p>
+            <p><strong>Telepon:</strong> +62 812-3456-7890</p>
+            <p><strong>Alamat:</strong> Jl.. raya padang no.123, mataram</p>
+        </div>
+        <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3945.094554264505!2d116.0921869!3d-8.586907299999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dcdbf7e3eff3f5b%3A0x34f884b732836131!2sUniversitas%20Mataram!5e0!3m2!1sid!2sid!4v1758332552415!5m2!1sid!2sid" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </section>
+    
+    <section class="testimoni">
+        <h2 style="text-align:center;">Testimoni Pelanggan</h2>
+        <p style="text-align:center;">Suara yang luar biasa dari pelanggan yang puas!</p>
+        <div class="testimoni-items">
+            <div class="testimoni-item">
+                <div class="stars">★★★★★</div>
+                <p>"makanan di sini selalu memuaskan dan enak!"</p>
+            </div>
+            <div class="testimoni-item">
+                <div class="stars">★★★★★</div>
+                <p>"Pelayanan ramah dan suasana yang nyaman."</p>
+                <p><strong>Siti Aminah</strong>, Pengunjung Bandung</p>
+            </div>
+            <div class="testimoni-item">
+                <div class="stars">★★★★★</div>
+                <p>"Harga yang terjangkau dan rasa yang menggugah."</p>
+                <p><strong>Budi Hartono</strong>, Konsumen Surabaya</p>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <p>© 2025 Rumah Makan Padang Sejahtera. All rights reserved.</p>
+        <p>
+            <a href="#">Kebijakan Privasi</a>
+            <a href="#">Syarat & Ketentuan</a>
+            <a href="#">Panduan Cookie</a>
+        </p>
+    </footer>
+
+</body>
+</html>
